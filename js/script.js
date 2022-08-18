8a779c04a776a11e3e1de093a1fe985ffb8928db
CJ <pinedacj1020@gmail.com>
2022-08-19 01:57:53 +0800

    grid works now with drag

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 const grid = document.querySelector(".grid");
 
-var size = 16;
+var size = 32;
+
+var height = window.innerHeight;
+var width = window.innerWidth;
+width = Math.sqrt(width);
+console.log(w,h);
 
 for(var i=0; i<size; i++){
     for(var j=0; j<size; j++){
         const row = document.createElement("div");
+        grid.style.gridTemplateRow = "repeat(32, 1fr)";
+        grid.style.gridTemplateColumns = "repeat(32, 1fr)"
         row.classList.add("row")
-        row.style.backgroundColor = "white";
-        row.style.height = "40px";
-        row.style.width = "40px";
+        row.style.height = "20px";
+        row.style.width = "20px";
         row.style.boxSizing = "border-box";
         grid.appendChild(row);
     }
@@ -17,15 +23,46 @@ for(var i=0; i<size; i++){
 const rows = document.querySelectorAll(".row");
 console.log(MouseEvent);
 
-grid.addEventListener('mousedown', (e) => {
+grid.addEventListener('mousedown', draw, false);
+grid.addEventListener('mouseup', stopDraw);
+
+function draw(){
     rows.forEach((item) => {
-            item.addEventListener('mouseenter', (e) => {
-                    e.target.style.backgroundColor = "black";
-            });
-    });
+        item.addEventListener('mouseenter', color);
+        item.addEventListener('mouseover', color);
+        item.addEventListener('mousemove',color)
 });
-grid.addEventListener('mouseup', (e) => {
+}
+
+function stopDraw(){
     rows.forEach((item) => {
-        item.removeEventListener('mouseenter');
+        item.removeEventListener('mouseenter', color);
+        item.removeEventListener('mouseover', color);
+        item.removeEventListener('mousemove', color);
 });
-})
+}
+var selectedColor = "black";
+function color(){
+   this.style.backgroundColor = selectedColor;
+}
+
+
+const eraser = document.querySelector("#erase");
+const colorSelector = document.querySelector("#colorSelector");
+
+eraser.addEventListener('click', erase);
+colorSelector.addEventListener('click', selectColor);
+
+function erase(){
+    rows.forEach((item) => {
+        item.style.backgroundColor = "white";
+});
+}
+
+function selectColor(){
+    selectedColor = "blue";
+}
+
+
+
+
