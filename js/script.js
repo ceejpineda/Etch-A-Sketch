8953c5233638604aa940a3c8d90a9b90b4ff8102
CJ <pinedacj1020@gmail.com>
2022-08-19 15:23:37 +0800

    added new styles and functions

--- js/script.js
+++ js/script.js
@@ -1,49 +1,74 @@
 const grid = document.querySelector(".grid");
+var gridElement = document.querySelectorAll(".gridElement");
 
-var size = 32;
-
-var height = window.innerHeight;
-var width = window.innerWidth;
-width = Math.sqrt(width);
-console.log(w,h);
-
-for(var i=0; i<size; i++){
-    for(var j=0; j<size; j++){
-        const row = document.createElement("div");
-        grid.style.gridTemplateRow = "repeat(32, 1fr)";
-        grid.style.gridTemplateColumns = "repeat(32, 1fr)"
-        row.classList.add("row")
-        row.style.height = "20px";
-        row.style.width = "20px";
-        row.style.boxSizing = "border-box";
-        grid.appendChild(row);
+function createGrid(){
+    dimensions = getDimensions();
+    heightGrid = Math.floor((dimensions[0])/1.5);
+    heightGrid = Math.floor(Math.sqrt(heightGrid))**2;
+    widthGrid = Math.floor((dimensions[1])/1.5);
+    widthGrid = Math.floor(Math.sqrt(widthGrid))**2
+    console.log(heightGrid,widthGrid);
+    pixelSize = Math.sqrt(heightGrid);
+    for(var i=0; i<pixelSize**2; i++){
+        const gridElement = document.createElement("div");
+        grid.style.height = `${heightGrid}px`;
+        grid.style.width = `${heightGrid}px`;
+        grid.style.gridTemplateRow = `repeat(${pixelSize}, 1fr)`;
+        grid.style.gridTemplateColumns = `repeat(${pixelSize}, 1fr)`;
+        gridElement.classList.add("gridElement");
+        gridElement.style.height = `${pixelSize}px`;
+        gridElement.style.width = `${pixelSize}px`;
+        gridElement.style.boxSizing = "border-box";
+        grid.appendChild(gridElement);
     }
+    window.addEventListener('resize', resizeGrid);
+    gridElement = document.querySelectorAll(".gridElement");
 }
 
-const rows = document.querySelectorAll(".row");
-console.log(MouseEvent);
+function getDimensions(){
+    var height = window.innerHeight;
+    var width = window.innerWidth;
+    width = Math.ceil(width);
+    height = Math.ceil(height);
+    var size = Math.min(height,width);
+    console.log(height,width,size);
+    return [height, width, size];
+}
+
+
+function resizeGrid(){
+    var child = grid.lastElementChild;
+    while(child){
+        grid.removeChild(child);
+        child = grid.lastElementChild;
+    }
+    createGrid();
+}    
+
+createGrid();
 
 grid.addEventListener('mousedown', draw, false);
 grid.addEventListener('mouseup', stopDraw);
 
 function draw(){
-    rows.forEach((item) => {
-        item.addEventListener('mouseenter', color);
-        item.addEventListener('mouseover', color);
-        item.addEventListener('mousemove',color)
+    gridElement.forEach((gridElements) => {
+        gridElements.addEventListener('mouseenter', color);
+        gridElements.addEventListener('mouseover', color);
+        gridElements.addEventListener('mousemove',color)
 });
 }
 
 function stopDraw(){
-    rows.forEach((item) => {
-        item.removeEventListener('mouseenter', color);
-        item.removeEventListener('mouseover', color);
-        item.removeEventListener('mousemove', color);
+    gridElement.forEach((gridElements) => {
+        gridElements.removeEventListener('mouseenter', color);
+        gridElements.removeEventListener('mouseover', color);
+        gridElements.removeEventListener('mousemove', color);
 });
 }
 var selectedColor = "black";
 function color(){
-   this.style.backgroundColor = selectedColor;
+    this.classList.add("colored");
+    this.style.backgroundColor = selectedColor;
 }
 
 
@@ -54,8 +79,8 @@ eraser.addEventListener('click', erase);
 colorSelector.addEventListener('click', selectColor);
 
 function erase(){
-    rows.forEach((item) => {
-        item.style.backgroundColor = "white";
+    gridElement.forEach((gridElements) => {
+        gridElements.style.backgroundColor = "white";
 });
 }
 
